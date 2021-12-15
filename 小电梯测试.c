d8fa4a45d9207cfe81ca944de2fff84f02b88202
kgjjd <2367149892@qq.com>
2021-12-15 16:13:21 +0800

    Add files via upload

---
+++ 小电梯测试.c
@@ -0,0 +1,14 @@
+﻿#include<stdio.h>
+#pragma warning (disable:4996)
+int main(void)
+{
+	int a;
+	printf("请输入你要到达的楼层：");
+	scanf("%d", &a);
+	for (a < 0 && a>22)
+	{
+		printf("对不起您输入的楼层不存在，请重新输入！");
+	}
+	printf("您已到达%d楼,祝您学习愉快！",a);
+	return 0;
+}
